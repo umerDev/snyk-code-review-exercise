2d2167c18a4e0b84adf371bca9e368bb528a9653
umer <umer.raja@bamgsports.com>
2025-03-03 16:41:09 +0000

    review: add comments for improvement

--- src/package.ts
+++ src/package.ts
@@ -3,6 +3,7 @@ import { maxSatisfying } from 'semver';
 import got from 'got';
 import { NPMPackage } from './types';
 
+// review: move to types file
 type Package = { version: string; dependencies: Record<string, Package> };
 
 /**
@@ -12,12 +13,15 @@ export const getPackage: RequestHandler = async function (req, res, next) {
   const { name, version } = req.params;
   const dependencyTree = {};
   try {
+    // review:  move to seperate function - get npm registry
     const npmPackage: NPMPackage = await got(
       `https://registry.npmjs.org/${name}`,
     ).json();
 
     const dependencies: Record<string, string> =
       npmPackage.versions[version].dependencies ?? {};
+
+    // review: can batch and execute in parallel using promise all
     for (const [name, range] of Object.entries(dependencies)) {
       const subDep = await getDependencies(name, range);
       dependencyTree[name] = subDep;
@@ -39,6 +43,7 @@ async function getDependencies(name: string, range: string): Promise<Package> {
   const v = maxSatisfying(Object.keys(npmPackage.versions), range);
   const dependencies: Record<string, Package> = {};
 
+  // review: check if v does not exist and return early
   if (v) {
     const newDeps = npmPackage.versions[v].dependencies;
     for (const [name, range] of Object.entries(newDeps ?? {})) {
